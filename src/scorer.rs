7c12c5b936603348bf6e1afffd652b2d212486be
Leonardo Vieira <jonaledi@gmail.com>
2024-12-02 10:53:33 -0300

    feat: simple contract struct

--- src/scorer.rs
+++ src/scorer.rs
@@ -1,1 +1,24 @@
+#[contracttype]
+#[derive(Clone, Debug, Eq, PartialEq)]
 
+// Structs and variables
+pub struct Test {
+    pub count: u32,
+    pub last_incr: u32,
+}
+
+// Contract struct
+#[contract]
+pub struct ScorerContract;
+
+// Contract implementation
+#[contractimpl]
+impl ScorerContract {
+    pub fn increment(env: Env, incr: u32) -> u32 {
+      let test = Test {
+        count: 0,
+        last_incr: 0,
+      };    
+      return test.count;
+    }
+}
